92d9b69203775247d66093c2b572513bf7d35dd3
BLooDBRothER <computertechnology446@gmail.com>
2022-06-25 08:23:04 +0530

    Create Header component

--- blog_app/src/components/Header.js
+++ blog_app/src/components/Header.js
@@ -1,15 +1,20 @@
 import React from 'react';
-import {FaUserAlt} from 'react-icons/fa';
+import { FaUserAlt } from 'react-icons/fa';
+import { Outlet } from 'react-router-dom';
 
 const Header = () => {
-  return (
-    <header>
-        <h1>GoBlogging</h1>
-        <div className='user'>
-            <FaUserAlt className='user-logo'/>
-        </div>
-    </header>
-  )
+    return (
+        <>
+            <header>
+                <h1>GoBlogging</h1>
+                <div className='user'>
+                    <FaUserAlt className='user-logo' />
+                </div>
+            </header>
+            <Outlet/>
+        </>
+
+    )
 }
 
 export default Header
