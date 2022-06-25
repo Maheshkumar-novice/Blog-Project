854e73995f9a6c3fd89a2a7792eaae080e406599
BLooDBRothER <computertechnology446@gmail.com>
2022-06-25 08:26:05 +0530

    Create routes with the component

--- blog_app/src/App.js
+++ blog_app/src/App.js
@@ -1,25 +1,22 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
+import Login from "./components/Login";
+import Register from "./components/Register";
+import Notification from "./components/Notification";
 
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Header/>} />
-        <Route index element={
-          <>
-            <div>hih</div>
-          </>
-        } />
-        <Route path="login"
-        element={
-          <>
-            <div>login</div>
-          </>
-        } />
+        <Route path="/" element={<Header />}>
+          <Route index element={<Notification message="test" variant="success"/>} />
+          <Route path="login" element={<Login />}/>
+          <Route path="register" element={<Register />} />
+        </Route>
       </Routes>
     </BrowserRouter>
+
   );
 }
 
