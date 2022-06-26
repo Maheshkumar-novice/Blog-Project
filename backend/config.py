cd65d56f9dc9995cd05fe5a80311f7a4c9f1a5a9
Maheshkumar <callmeelprofessor@gmail.com>
2022-06-26 12:21:50 +0530

    Fix: Session cookie setting

--- backend/config.py
+++ backend/config.py
@@ -2,6 +2,8 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
+    SESSION_COOKIE_SAMESITE = 'None'
+    SESSION_COOKIE_SECURE = 'True'
     REMEMBER_COOKIE_SAMESITE = 'None'
     REMEMBER_COOKIE_SECURE = 'True'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
